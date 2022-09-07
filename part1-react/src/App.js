400c16d8d10af6f95e59c4c23c2bbffeaa10174d
ali-hasan97 <ali_hasan97@hotmail.com>
2022-09-06 21:35:37 -0500

    condensed variables into one object

--- part1-react/src/App.js
+++ part1-react/src/App.js
@@ -7,48 +7,44 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1.name} {props.part1.exercises}</p>
-      <p>{props.part2.name} {props.part2.exercises}</p>
-      <p>{props.part3.name} {props.part3.exercises}</p>
+      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
+      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
+      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
     </div>
   )
 };
 
 const Total = (props) => {
-  let sum = props.exercises1 + props.exercises2 + props.exercises3;
+  let sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
   return (
     <p>Number of exercises {sum}</p>
   )
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-      part1={part1}
-      part2={part2}
-      part3={part3}
-      />
-      <Total 
-      exercises1={part1.exercises} 
-      exercises2={part2.exercises} 
-      exercises3={part3.exercises}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 };
