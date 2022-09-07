f1e349cb4747897c17a61807087615139c904d92
ali-hasan97 <ali_hasan97@hotmail.com>
2022-09-07 16:10:44 -0500

    created Notes app

---
+++ part2-servercomm/collection-modules/notes-app/src/App.js
@@ -0,0 +1,13 @@
+import Note from "./components/Note"
+
+const App = ({ notes }) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map(note => <Note note={note} key={note.id}/>)}
+    </div>
+  )
+}
+
+export default App;
