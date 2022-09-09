f6858ffd7641a65e4e3fd4384d5c9384d24c777d
ali-hasan97 <ali_hasan97@hotmail.com>
2022-09-09 05:19:04 -0500

    added 'show' buttons to allow users to jump to a country directly once it is in the list of countries

--- part2-servercomm/countries/src/components/CountryList.js
+++ part2-servercomm/countries/src/components/CountryList.js
@@ -1,7 +1,14 @@
-const CountryList = ({ name }) => {
+const CountryList = ({ name, setSearch, search }) => {
+    const handleShow = (event) =>  {
+        setSearch(event.target.id)
+        console.log(search)
+        console.log(event.target.id)
+    }
+
     return (
         <tr>
             <td>{name}</td>
+            <td><button id={name} key={name} onClick={(event) => handleShow(event)}>show</button></td>
         </tr>
         );    
 }
--- part2-servercomm/countries/src/components/Search.js
+++ part2-servercomm/countries/src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import CountryList from "./CountryList";
 import SingleView from "./SingleView";
 
 const Search = ({ countries }) => {
   const [search, setSearch] = useState("");
-  const [filtered, setFiltered] = useState([])
-  const [display, setDisplay] = useState(['Be more specific'])
+  const [filtered, setFiltered] = useState([]);
+  const [display, setDisplay] = useState(["Be more specific"]);
 
   useEffect(() => {
     setFiltered(
@@ -17,44 +17,45 @@ const Search = ({ countries }) => {
   }, [search, countries]);
 
   useEffect(() => {
-    if  (filtered.length > 20) {
-      setDisplay('Be more specific')
+    if (filtered.length > 20) {
+      setDisplay("Be more specific");
     } else if (filtered.length === 1) {
-      setDisplay('single')
+      setDisplay("single");
     } else {
-      setDisplay('filtered')
+      setDisplay("filtered");
     }
-    console.log(display)
-    }, [[]]);
+  }, [[]]);
 
   const displayMethod = () => {
-    if (display === 'filtered') {
+    if (display === "filtered") {
       return (
         <table>
           <tbody>
-            {filtered.slice(0,20).map(country => {
+            {filtered.slice(0, 20).map((country) => {
               return (
-                      <CountryList name={country.name.common} key={country.name.common} />
-              )
-            })
-            }
+                <CountryList
+                  name={country.name.common}
+                  key={country.name.common}
+                  setSearch={setSearch}
+                  search={search}
+                />
+              );
+            })}
           </tbody>
         </table>
-      )
-    } else if (display === 'single') {
-        return (
-          <div>
-            {filtered.map(country => {
-              return (
-                <SingleView country={country} key={country.name.common} />
-              )
-            })}
-          </div>
-        )
+      );
+    } else if (display === "single") {
+      return (
+        <div>
+          {filtered.map((country) => {
+            return <SingleView country={country} key={country.name.common} />;
+          })}
+        </div>
+      );
     } else {
-      return <h2>Be more specific!</h2>
+      return <h2>Be more specific!</h2>;
     }
-  }
+  };
 
   return (
     <div>
